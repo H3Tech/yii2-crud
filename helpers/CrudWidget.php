50fdc2ab1b0881b2b25212e1028ea873b0b743f2
Gyula Szabó <gt.szgyula@gmail.com>
2018-05-26 16:35:56 +0200

    Add widget for single media attribute display

--- helpers/CrudWidget.php
+++ helpers/CrudWidget.php
@@ -7,38 +7,58 @@ use kartik\file\FileInput;
 
 class CrudWidget
 {
-    public static function multipleMediaDisplayAttribute($attribute, $junctionClass, $mediaIdField, $modelIdField)
+    protected static function getDisplayAttribute($attribute, $valueFunction)
     {
         return [
             'attribute' => $attribute,
             'format' => 'raw',
-            'value' => function ($model) use ($attribute, $junctionClass, $mediaIdField, $modelIdField) {
-                $preview = MediaController::getMultiplePreviewData(
-                    $model->primaryKey,
-                    $junctionClass,
-                    $modelIdField,
-                    $mediaIdField
-                );
-
-                return FileInput::widget([
-                    'model' => $model,
-                    'attribute' => $attribute,
-                    'pluginOptions' => [
-                        'showClose' => false,
-                        'initialPreviewAsData' => true,
-                        'initialPreview' => $preview['initialPreview'],
-                        'initialPreviewConfig' => $preview['initialPreviewConfig'],
-                        'fileActionSettings' => [
-                            'showDrag' => false,
-                            'showRemove' => false,
-                            'showZoom' => false,
-                        ],
-                        'showCaption' => false,
-                        'browseClass' => 'hidden',
-                    ],
-                    'readonly' => true,
-                ]);
-            }
+            'value' => $valueFunction,
         ];
+    }
+
+    protected static function getMediaDisplayWidget($model, $attribute, $preview)
+    {
+        return FileInput::widget([
+            'model' => $model,
+            'attribute' => $attribute,
+            'pluginOptions' => [
+                'showClose' => false,
+                'initialPreviewAsData' => true,
+                'initialPreview' => $preview['initialPreview'],
+                'initialPreviewConfig' => $preview['initialPreviewConfig'],
+                'fileActionSettings' => [
+                    'showDrag' => false,
+                    'showRemove' => false,
+                    'showZoom' => false,
+                ],
+                'showCaption' => false,
+                'browseClass' => 'hidden',
+                'dropZoneTitle' => '',
+            ],
+            'readonly' => true,
+        ]);
+    }
+
+    public static function mediaDisplayAttribute($attribute)
+    {
+        return static::getDisplayAttribute($attribute, function ($model) use ($attribute, $junctionClass, $mediaIdField, $modelIdField) {
+            return static::getMediaDisplayWidget($model, $attribute, MediaController::getSinglePreviewData(
+                $model->$attribute,
+                $attribute,
+                get_class($model)
+            ));
+        });
+    }
+
+    public static function multipleMediaDisplayAttribute($attribute, $junctionClass, $mediaIdField, $modelIdField)
+    {
+        return static::getDisplayAttribute($attribute, function ($model) use ($attribute, $junctionClass, $mediaIdField, $modelIdField) {
+            return static::getMediaDisplayWidget($model, $attribute, MediaController::getMultiplePreviewData(
+                $model->primaryKey,
+                $junctionClass,
+                $modelIdField,
+                $mediaIdField
+            ));
+        });
     }
 }
