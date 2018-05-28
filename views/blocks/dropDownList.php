8db23d24041fbaba63e160914768da416bb1033d
Gyula Szabó <gt.szgyula@gmail.com>
2018-05-28 13:36:18 +0200

    Add ability to allow empty value in case of dropdowns

--- views/blocks/dropDownList.php
+++ views/blocks/dropDownList.php
@@ -2,4 +2,8 @@
 
 $hint = isset($settings['hint']) ? $settings['hint'] : null;
 
-echo $form->field($model, $field)->dropDownList($settings["items"], $settings)->hint($hint);
+echo $form->field($model, $field)->dropDownList($settings['items'], [
+    'prompt' => isset($settings['allowEmptyValue']) && $settings['allowEmptyValue']
+        ? Yii::t('h3tech/crud/crud', 'None')
+        : null,
+])->hint($hint);
