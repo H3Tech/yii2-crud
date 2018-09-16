ee7c93e6b696b4eba8f6f2321c9fc228497659ab
Gyula Szabó <gt.szgyula@gmail.com>
2018-09-16 18:05:23 +0200

    Make datetime picker block more configurable

--- views/blocks/dateTimePicker.php
+++ views/blocks/dateTimePicker.php
@@ -5,13 +5,18 @@ use kartik\datetime\DateTimePicker;
 /* @var string $field */
 
 $hint = isset($settings['hint']) ? $settings['hint'] : null;
+$options = isset($settings['options']) ? $settings['options'] : [];
+$pluginOptions = isset($settings['pluginOptions']) ? $settings['pluginOptions'] : [];
+
 
 echo $form->field($model, $field)->widget(DateTimePicker::className(), [
     'name' => $field,
-    'options' => ['placeholder' => Yii::t('h3tech/crud/crud', 'Select time...')],
+    'options' => array_merge([
+        'placeholder' => Yii::t('h3tech/crud/crud', 'Select time...'),
+    ], $options),
     'readonly' => true,
-    'pluginOptions' => [
+    'pluginOptions' => array_merge([
         'format' => 'yyyy-mm-dd HH:ii',
         'autoclose' => true,
-    ],
+    ], $pluginOptions),
 ])->hint($hint);
