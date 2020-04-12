c66124b4774a4058c017274419c52dd0d3e690eb
Gyula Szabó <gt.szgyula@gmail.com>
2020-04-12 09:19:24 +0200

    Fix checkbox label duplication

--- views/blocks/checkbox.php
+++ views/blocks/checkbox.php
@@ -1,8 +1,5 @@
 <?php
 
-$label = isset($settings['label']) ? $settings['label'] : null;
-$labelOptions = isset($settings['labelOptions']) ? $settings['labelOptions'] : [];
-
 $hint = isset($settings['hint']) ? $settings['hint'] : null;
 
-echo $form->field($model, $field)->checkbox($settings)->hint($hint)->label($label, $labelOptions);
+echo $form->field($model, $field)->checkbox($settings)->hint($hint);
